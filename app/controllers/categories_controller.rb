10c632a728813d8787b72df48bb5830ec41dd670
Joe Jobes <jmrjobes@gmail.com>
2018-02-05 16:58:40 -0700

    Add create and show action to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,35 @@
 class CategoriesController < ApplicationController
 
+  def show
+    @category = Category.find(params[:id])
+  end
+
   def index
     @categories = Category.all
   end
 
   def new
-    binding.pry
     @category = Category.new
   end
 
   def create
+    binding.pry
+    @category = Category.new(category_params)
+    if Category.exists?(title: params[:title])
+      render :new
+      flash[:alert] = "Category already exists"
+    elsif @category.save
+      flash[:success] = "You have successfully created a category"
+      redirect category_path(@category)
+    else
+      render :new
+      flash[:alert] = "Error! Category not created."
+    end
   end
 
   private
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:title)
   end
 
 end
