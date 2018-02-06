a3505374291f97a7047e19b27afd6b9c5312a5fa
Joe Jobes <jmrjobes@gmail.com>
2018-02-06 06:35:17 -0700

    Add edit category feature test

---
+++ spec/features/categories/user_can_edit_a_category_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "a user edits a category" do
+  describe "user navigates to categories index" do
+    scenario "user edits a category" do
+      category = Category.create!(title: "Panda Care")
+
+      visit categories_path
+
+      click_on "Edit"
+
+      fill_in 'category[title]', with: "Red Panda Grooming"
+      click_on "Edit Category"
+
+      expect(page).to have_content("Red Panda Grooming")
+    end
+  end
+end
