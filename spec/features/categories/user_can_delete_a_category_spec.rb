3aa4abffdcc5a4466767bd312dda1d60d06185ac
Joe Jobes <jmrjobes@gmail.com>
2018-02-06 06:55:56 -0700

    Add delete link to category index view

--- spec/features/categories/user_can_delete_a_category_spec.rb
+++ spec/features/categories/user_can_delete_a_category_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 describe "a user can delete a category" do
   describe "user navigates to category index" do
     scenario "user deletes category" do
-      category = Category.create!(title: "LADDERS")
-      category = Category.create!(title: "Enuma Elish")
+      category1 = Category.create!(title: "LADDERS")
+      category2 = Category.create!(title: "Enuma Elish")
 
       visit categories_path
+      find(".LADDERS").click_on "Delete"
 
-      click_on "Delete"
-
-      expect(page).to_not have_content("LADDERS")
-      expect(page).to have_content("LADDERS")
+      expect(page).to_not have_content(category1.title)
+      expect(page).to have_content(category2.title)
     end
   end
 end
