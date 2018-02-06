861b1403b3b0166d1baca68a03cc8ccdf0cb277e
Joe Jobes <jmrjobes@gmail.com>
2018-02-06 06:33:24 -0700

    Add test for if user creates a category that already exists

--- spec/features/categories/user_can_create_a_category_spec.rb
+++ spec/features/categories/user_can_create_a_category_spec.rb
@@ -9,4 +9,14 @@ describe "user creates new category" do
 
     expect(page).to have_content("Music Production")
   end
+
+  scenario "user tries to create category that already exists" do
+    Category.create!(title: 'Author')
+    visit new_category_path
+
+    fill_in "category[title]", with: "Author"
+    click_on "Create Category"
+    expect(current_path).to eq(new_category_path)
+    expect(page).to have_content("Category already exists")
+  end
 end
