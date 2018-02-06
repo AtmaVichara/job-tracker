f0ee576f02822264b3de68ae3c9264ff60dd9c91
Joe Jobes <jmrjobes@gmail.com>
2018-02-06 06:34:08 -0700

    Add edgecase for categories already made, and start on edit action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,19 +13,27 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    binding.pry
     @category = Category.new(category_params)
-    if Category.exists?(title: params[:title])
-      render :new
-      flash[:alert] = "Category already exists"
+    if Category.exists?(title: params[:category][:title])
+      redirect_to new_category_path
+      flash[:alert] = "Category already exists! Please try another."
     elsif @category.save
       flash[:success] = "You have successfully created a category"
-      redirect category_path(@category)
+      redirect_to category_path(@category)
     else
       render :new
       flash[:alert] = "Error! Category not created."
     end
   end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+  
 
   private
   def category_params
